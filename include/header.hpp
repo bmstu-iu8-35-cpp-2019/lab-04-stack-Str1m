e7fb04313ecf6af73b28073564bc3ad4ccf45632
Str1m <q.st.stream@gmail.com>
2019-10-07 18:15:19 +0300

    First try

--- include/header.hpp
+++ include/header.hpp
@@ -1,6 +1,97 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2019 Sabitov Egor <q.st.stream@gmail.com>
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
 
+#include <iostream>
+
+class Stack {
+private:
+    size_t num;
+    size_t size;
+    std::unique_ptr<T[]> data;
+public:
+    Stack() {
+        num = 0;
+        size = 1;
+        data = std::make_unique<T[]>(size);
+    }
+
+    Stack(const Stack<T> &tmp) = delete;
+
+    Stack(Stack<T> &&tmp) noexcept {
+        data = std::move(tmp._data);
+        tmp._data = nullptr;
+        size = tmp._capacity;
+        num = tmp._size;
+    }
+
+    ~Stack() {}
+
+    void push(T &&value) {
+        if (num == size) {
+            num++;
+            size = 3 / 2 * num;
+            auto buf = std::make_unique<T[]>(size);
+            for (size_t i = 0; i < num; i++)
+                buf[i] = data[i];
+            buf[num - 1] = value;
+            data = std::move(buf);
+        } else {
+            num++;
+            data[num - 1] = value;
+        }
+    }
+
+    void push(const T &value) {
+        if (num == size) {
+            num++;
+            size = 3 / 2 * num;
+            auto buf = std::make_unique<T[]>(size);
+            for (size_t i = 0; i < num; i++)
+                buf[i] = data[i];
+            buf[num] = value;
+            data = std::move(buf);
+        } else {
+            num++;
+            data[num] = value;
+        }
+    }
+
+    void pop() {
+        if (size != 0)
+            num--;
+        else
+            throw std::logic_error("Out of range");
+    }
+
+    const T &head() const {
+        return data[num - 1];
+    }
+
+    template<typename ... Args>
+    void push_emplace(Args &&... value) {
+        if (num == size) {
+            num++;
+            size = 3 / 2 * num;
+            auto buf = std::make_unique<T[]>(size);
+            for (size_t i = 0; i < num; i++)
+                buf[i] = data[i];
+            buf[num - 1] = T(value ...);
+            data = std::move(buf);
+        } else {
+            num++;
+            data[num - 1] = T(value ...);
+        }
+    }
+
+    T pop(int) {
+        if (size != 0)
+            num--;
+        else
+            throw std::logic_error("Out of range");
+        return data[num];
+    }
+
+};
 #endif // INCLUDE_HEADER_HPP_
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,38 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2019 Sabitov Egoк <q.st.stream@gmail.com>
 
 #include <gtest/gtest.h>
 
 #include <header.hpp>
+
+TEST (Stack_, Test_1){
+    Stack<int> a;
+    a.push(1);
+    a.push(2);
+    a.push(3);
+    a.push(4);
+    int val = a.pop(1);
+    EXPECT_EQ(4,val);
+}
+
+TEST (Stack_, Test_2){
+    Stack<int> a;
+    a.push(1);
+    a.push(2);
+    a.push(3);
+    a.push(4);
+    int val = a.head();
+    EXPECT_EQ(4,val);
+}
+
+TEST (Stack_, Test_3){
+    Stack<int> a;
+    a.push(1);
+    a.push(2);
+    a.push(3);
+    a.push(4);
+    int val = a.head();
+    EXPECT_EQ(4,val);
+    a.push(5);
+    int val = a.head();
+    EXPECT_EQ(5,val);
+}
